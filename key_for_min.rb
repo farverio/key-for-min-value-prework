41409063839c9e605f48a82dcad0a292d2ad5a70
Stephen F <stephen@farver.io>
2018-05-30 16:41:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,9 +9,10 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     smallest_key = nil
   else name_hash.map do |key, value|
-    if value < smallest_value
-      smallest_value = value
-      smallest_key = key
+      if value < smallest_value
+        smallest_value = value
+        smallest_key = key
+      end
     end
   end
   
